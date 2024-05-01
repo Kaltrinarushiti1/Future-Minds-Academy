123e84e8f20f00a177077899a673146f5c9ae603
Kaltrinarushiti1 <kaltrinarushiti676@gmail.com>
2024-05-01 14:35:57 +0200

    template

---
+++ FMACSS-Framework/assets/js/template.js
@@ -0,0 +1,27 @@
+function changeImageRight() {
+    var img = document.getElementById('imagee');
+    img.src = 'assets/img/images/home2.jpg';
+}
+
+function changeImageLeft() {
+    var img = document.getElementById('imagee');
+    img.src = 'assets/img/images/home-img.jpg';
+}
+
+window.addEventListener('scroll',reveal);
+function reveal(){
+    let reveals = document.querySelectorAll('.reveal');
+    for(let i = 0; i < reveals.length; i++ ){
+        let windowheight = window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        let revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+
+}
